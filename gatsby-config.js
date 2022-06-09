5e7fd381afaacbfb92cb9a24c9dc9f222cea5fd6
dekzzie <perecadrian@gmail.com>
2022-06-09 15:41:43 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `portfolio-pees`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-datocms',
+    options: {
+      "apiToken": "e67639407efa38257ebca8488ab9e9"
+    }
+  }, "gatsby-plugin-styled-components"]
+};
