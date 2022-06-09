8802ca501ea763674088173e55872a04fe944bc6
dekzzie <perecadrian@gmail.com>
2022-06-09 16:34:13 +0200

    main : add global styles and create layout component

---
+++ src/components/Layout/layout.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { theme } from '../../helpers/theme';
+
+const GlobalStyle = createGlobalStyle`
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 16px;
+  scroll-behavior: smooth;
+
+  ::-moz-selection {
+    /* Code for Firefox */
+    color: ${({ theme }) => theme.colors.black};
+    background: ${({ theme }) => theme.colors.white};
+  }
+
+  ::selection {
+    color: ${({ theme }) => theme.colors.black};
+    background: ${({ theme }) => theme.colors.white};
+}}
+
+body {
+  background-color: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.white};
+}
+
+`;
+
+export default function Layout({ children }) {
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			{children}
+		</ThemeProvider>
+	);
+}
