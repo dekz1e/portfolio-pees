6db09fb2755e2e2429e6cfd8117d4976824d9098
dekzzie <perecadrian@gmail.com>
2022-07-11 11:54:42 +0200

    update seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,23 @@
 module.exports = {
 	siteMetadata: {
-		title: `portfolio-pees`,
+		title: `Mush House`,
+		titleTemplate: '%s - Produkty tworzone na miare',
+		description: `Projektujemy oraz wdrażamy strony, sklepy oraz aplikacje
+		internetowe. Zajmujemy się również tworzeniem różnego rodzaju
+		materiałów graficznych.`,
+		url: 'https://www.mushouse.pl',
 		siteUrl: `https://www.mushouse.pl`,
+		twitterUsername: '@mushousepl',
+		facebookUsername: '@mushousepl',
+		instagramUsername: '@mushousepl',
+		image: '/src/images/icon2.png',
+		icon: '/src/images/icon2.png'
 	},
 	plugins: [
 		{
 			resolve: 'gatsby-source-datocms',
 			options: {
 				apiToken: 'e67639407efa38257ebca8488ab9e9',
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				name: 'Mush House - Produkty tworzone na essie!',
-				short_name: 'Mush House',
-				description: `Projektujemy oraz wdrażamy strony, sklepy oraz aplikacje
-				internetowe. Zajmujemy się również tworzeniem różnego rodzaju
-				materiałów graficznych.`,
-				lang: 'pl',
-				icon: './src/images/icon2.png',
-				start_url: '/',
 			},
 		},
 		'gatsby-plugin-styled-components',
