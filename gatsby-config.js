308bfda2bbb91a1743e91e9c30805d08462fc4df
dekzzie <perecadrian@gmail.com>
2022-07-11 11:58:54 +0200

    add manifest gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,14 +10,25 @@ module.exports = {
 		twitterUsername: '@mushousepl',
 		facebookUsername: '@mushousepl',
 		instagramUsername: '@mushousepl',
-		image: '/src/images/icon2.png',
-		icon: '/src/images/icon2.png'
 	},
 	plugins: [
 		{
 			resolve: 'gatsby-source-datocms',
 			options: {
 				apiToken: 'e67639407efa38257ebca8488ab9e9',
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'Mush House - Produkty tworzone na essie!',
+				short_name: 'Mush House',
+				description: `Projektujemy oraz wdrażamy strony, sklepy oraz aplikacje
+				internetowe. Zajmujemy się również tworzeniem różnego rodzaju
+				materiałów graficznych.`,
+				lang: 'pl',
+				icon: './src/images/icon2.png',
+				start_url: '/',
 			},
 		},
 		'gatsby-plugin-styled-components',
